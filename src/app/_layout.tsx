5ad9f6ab03658af57314ac6763a106d16f90375d
Adefemi <adefemiadeyanju101@hotmail.com>
2026-04-06 08:42:37 +0100

    corrected layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,15 @@
 // src/app/_layout.tsx
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar"; // Add this import
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      {/* Add StatusBar component here */}
+      <StatusBar style="dark" hidden={false} />
+
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
